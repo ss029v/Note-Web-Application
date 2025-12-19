c902693c98eb47ef4a25abf329689d73b3a0e5d1
Shreyash Vairagade <shreyashvairagadegithub29@gmail.com>
2025-12-19 13:51:16 +0530

    Rename test class for consistency

--- notes_app_backend/src/test/java/com/notesapp/NotesAppBackendApplicationTests.java
+++ notes_app_backend/src/test/java/com/notesapp/NotesAppBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotesAppBackendApplicationTests {
+class NotesAppBackendApplicationTest {
 
 	@Test
 	void contextLoads() {
